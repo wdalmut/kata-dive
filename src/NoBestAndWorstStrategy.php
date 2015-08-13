5bcfc4ec74b14b39a3fb742261e93afb50e9697f
Walter Dal Mut <walter.dalmut@gmail.com>
2015-08-13 16:47:08 +0200

    Added base input validation

--- src/NoBestAndWorstStrategy.php
+++ src/NoBestAndWorstStrategy.php
@@ -5,6 +5,10 @@ class NoBestAndWorstStrategy implements VotingStrategy
 {
     public function votes(array $votes, Dive $dive)
     {
+        if (count($votes) < 3) {
+            throw new \InvalidArgumentException("We need at least 3 votes");
+        }
+
         unset($votes[array_search(min($votes), $votes)]);
         unset($votes[array_search(max($votes), $votes)]);
 
--- tests/NoBestAndWorstStrategyTest.php
+++ tests/NoBestAndWorstStrategyTest.php
@@ -25,6 +25,26 @@ class NoBestAndWorstStrategyTest extends \PHPUnit_Framework_TestCase
             [[9,8,8,8,7], new Dive(1), 24],
             [[9,8,8,8,7], new Dive(2), 48],
             [[9,8,8,8,9], new Dive(2), 50],
+            [[8,8,8], new Dive(2), 16], //Lowest Boundary
+            [[6,8,9], new Dive(2), 16], //Lowest Boundary
+        ];
+    }
+
+    /**
+     * @dataProvider wrongVoters
+     * @expectedException InvalidArgumentException
+     */
+    public function testInvalidVotersNumber($voters)
+    {
+        $this->strategy->votes($voters, new Dive(1));
+    }
+
+    public function wrongVoters()
+    {
+        return [
+            [[]],
+            [[1,]],
+            [[1,2]],
         ];
     }
 }
